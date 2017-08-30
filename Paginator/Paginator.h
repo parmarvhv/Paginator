bdc8f9c4256c5dcffd415ddd7f6f399faf61e917
Vaibhav Parmar <vaibhav@nickelfox.com>
2017-08-30 12:38:43 +0530

    Initial Commit

---
+++ Paginator/Paginator.h
@@ -0,0 +1,19 @@
+//
+//  Paginator.h
+//  Paginator
+//
+//  Created by Vaibhav Parmar on 30/08/17.
+//  Copyright © 2017 Vaibhav Parmar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Paginator.
+FOUNDATION_EXPORT double PaginatorVersionNumber;
+
+//! Project version string for Paginator.
+FOUNDATION_EXPORT const unsigned char PaginatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Paginator/PublicHeader.h>
+
+
